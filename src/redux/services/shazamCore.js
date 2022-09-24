bf290503d5425eb724727625b35cdd54befe4c45
ellisvelandia <eyis619@gmail.com>
2022-09-23 22:25:13 -0500

    spotify

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set('X-RAPIDAPI-Key', 'f7c77fb172msh1ef00876340a814p154163jsn8055208d264a');
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  }),
+});
+
+export const {
+  useGetTopChartsQuery,
+} = shazamCoreApi;
